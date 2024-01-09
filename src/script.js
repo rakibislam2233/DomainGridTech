742c37752568702446866af1fd7c10d33e271cec
rakibislam2233 <rakib2020.tkg@gmail.com>
2024-01-09 12:58:52 +0600

    navbar toogle added

--- src/script.js
+++ src/script.js
@@ -1,1 +1,6 @@
-console.log('object')
+const rakib = document.getElementById('mobile-menu');
+document.getElementById('mobile-menu').addEventListener('click', function () {
+   var navbarList = document.getElementById('navbar-list');
+   navbarList.classList.toggle('show');
+ });
+
