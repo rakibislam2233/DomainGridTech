c19c0401d8c2ac0c56c963840f0ce1dcdb3dd0da
rakibislam2233 <rakib2020.tkg@gmail.com>
2024-01-09 13:02:37 +0600

    project completed

--- src/script.js
+++
@@ -1,6 +0,0 @@
-const rakib = document.getElementById('mobile-menu');
-document.getElementById('mobile-menu').addEventListener('click', function () {
-   var navbarList = document.getElementById('navbar-list');
-   navbarList.classList.toggle('show');
- });
-
